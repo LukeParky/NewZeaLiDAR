78f19ee56805cb606f843a0607ada5eab5e630c3
Casey Li <casey.li@canterbury.ac.nz>
2024-07-30 13:43:39 +1200

    update `env_var.py` to ensure consistency with DT `config.py`

--- newzealidar/datasets.py
+++ newzealidar/datasets.py
@@ -401,7 +401,7 @@ def store_file_to_s3(execution_time):
     Store files in an AWS S3 bucket if their last modified timestamp is newer than the module's execution time.
     """
     # Retrieve the value of the environment variable "USE_AWS_S3_BUCKET"
-    use_aws_s3_bucket = env_var.get_bool_env_variable("USE_AWS_S3_BUCKET", allow_empty=True, default=False)
+    use_aws_s3_bucket = env_var.get_bool_env_variable("USE_AWS_S3_BUCKET", default=False)
     if use_aws_s3_bucket:
         # Get the directory containing the files to upload
         data_dir = pathlib.Path(env_var.get_env_variable("DATA_DIR")) / pathlib.Path(
--- newzealidar/process.py
+++ newzealidar/process.py
@@ -61,7 +61,7 @@ def save_instructions(instructions: dict, instructions_path: str) -> None:
     with open(instructions_path, "w") as f:
         json.dump(instructions, f, default=_recursive_str, indent=2)
     # Retrieve the value of the environment variable "USE_AWS_S3_BUCKET"
-    use_aws_s3_bucket = env_var.get_bool_env_variable("USE_AWS_S3_BUCKET", allow_empty=True, default=False)
+    use_aws_s3_bucket = env_var.get_bool_env_variable("USE_AWS_S3_BUCKET", default=False)
     if use_aws_s3_bucket:
         s3_manager = S3Manager()
         s3_manager.store_file(s3_object_key=instructions_path, file_path=instructions_path)
@@ -175,7 +175,7 @@ def store_raw_files_to_s3(s3_manager, s3_objects, instructions: dict):
 
 def store_raw_dem_to_s3(instructions: dict) -> None:
     # Retrieve the value of the environment variable "USE_AWS_S3_BUCKET"
-    use_aws_s3_bucket = env_var.get_bool_env_variable("USE_AWS_S3_BUCKET", allow_empty=True, default=False)
+    use_aws_s3_bucket = env_var.get_bool_env_variable("USE_AWS_S3_BUCKET", default=False)
     if use_aws_s3_bucket:
         s3_manager = S3Manager()
         s3_objects = s3_manager.list_objects()
@@ -186,7 +186,7 @@ def store_raw_dem_to_s3(instructions: dict) -> None:
 
 def store_hydro_dem_to_s3(instructions: dict) -> None:
     # Retrieve the value of the environment variable "USE_AWS_S3_BUCKET"
-    use_aws_s3_bucket = env_var.get_bool_env_variable("USE_AWS_S3_BUCKET", allow_empty=True, default=False)
+    use_aws_s3_bucket = env_var.get_bool_env_variable("USE_AWS_S3_BUCKET", default=False)
     if use_aws_s3_bucket:
         s3_manager = S3Manager()
         s3_objects = s3_manager.list_objects()
@@ -552,7 +552,7 @@ def main(
         / pathlib.Path("lidar_extent.gpkg")
     )
     # Retrieve the value of the environment variable "USE_AWS_S3_BUCKET"
-    use_aws_s3_bucket = env_var.get_bool_env_variable("USE_AWS_S3_BUCKET", allow_empty=True, default=False)
+    use_aws_s3_bucket = env_var.get_bool_env_variable("USE_AWS_S3_BUCKET", default=False)
     if use_aws_s3_bucket:
         s3_manager = S3Manager()
         s3_objects = s3_manager.list_objects()
